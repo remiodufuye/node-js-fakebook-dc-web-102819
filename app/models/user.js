272f4e719d6a2bd316bfe61ce429d1a3c99e6cdd
github username <github email address>
2020-03-14 08:45:09 -0400

    Done.

--- app/models/user.js
+++ app/models/user.js
@@ -10,6 +10,7 @@ const User = bookshelf.Model.extend({
   tableName: 'users',
   initialize: function() {
     this.on('creating', this.encryptPassword);
+    this.on('destroying', this.destroyAllAttached);
   },
   hasTimestamps: true,
   posts: function() {
@@ -32,6 +33,18 @@ const User = bookshelf.Model.extend({
         resolve(hash);
       });
     });
+  },
+  destroyAllAttached: function(model, options) {
+    return Promise.all([
+      bookshelf
+        .knex('users_users')
+        .where('user_id', model.get('id'))
+        .delete(),
+      bookshelf
+        .knex('users_users')
+        .where('follower_id', model.get('id'))
+        .delete()
+    ]);
   },
   validatePassword: function(suppliedPassword) {
     let self = this;
